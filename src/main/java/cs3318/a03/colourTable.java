d6f3114db6b915f4b9f44447d0db9a3816182b14
AmyCraven <92314129+AmyCraven@users.noreply.github.com>
2023-11-26 13:06:40 +0000

    Testing palette size. Invalid test is failing

--- src/main/java/cs3318/a03/colourTable.java
+++ src/main/java/cs3318/a03/colourTable.java
@@ -1,4 +1,23 @@
 package cs3318.a03;
 
+import java.util.ArrayList;
+
 public class colourTable {
+    final int paletteSize;
+    final ArrayList<int[]> palette;
+
+    public colourTable(int size) {
+        if (isValidPaletteSize(size)) {
+            this.paletteSize = size;
+            this.palette = new ArrayList<>(size);
+        } else {
+            throw new IllegalArgumentException("Invalid palette size");
+        }
+    }
+
+    boolean isValidPaletteSize(int size) {
+        return size > 1 && size < 1025;
+    }
+
+
 }
--- src/test/java/cs3318/a03/colourTableTest.java
+++ src/test/java/cs3318/a03/colourTableTest.java
@@ -1,9 +1,11 @@
 package cs3318.a03;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class colourTableTest {
@@ -15,6 +17,15 @@ public class colourTableTest {
         if (myTable.isValidPaletteSize(paletteSize)) {
             assertTrue(myTable.isValidPaletteSize(paletteSize));
         }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {3, 1030, 5})
+    public void invalidPaletteSizeTest(int paletteSize) {
+
+        Executable colourTableConstructor = () -> new colourTable(paletteSize);
+        assertThrows(IllegalArgumentException.class, colourTableConstructor,
+                "Invalid palette size");
     }
 
 }
